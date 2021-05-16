309c53994adde5fba609b8f48df62d10453a9b68
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-05-16 10:31:51 +0700

    [Bernard] Add fields to SkpkDataSuratDisplay

--- src/Components/SkpkFormDisplayCards/SkpkDataSuratDisplay.js
+++ src/Components/SkpkFormDisplayCards/SkpkDataSuratDisplay.js
@@ -8,6 +8,12 @@ const SkmkDataSurat = (props) => {
     nomorSurat,
     tanggalSurat,
     namaPenandatangan,
+    namaRsPkm,       
+    kodeRsPkm,       
+    nomorUrutSurat,  
+    nomorRekamMedis, 
+    namaPenerima,    
+    hubunganPenerima,
   } = props;
   
   return (
@@ -41,10 +47,50 @@ const SkmkDataSurat = (props) => {
       </Row>
       <Row gutter={16}>
         <Col span={6}>
-          <p>Petugas yang Menandatangani Surat</p>
+          <p>Nama RS/PKM</p>
         </Col>
         <Col span={18}>
-          <p>{namaPenandatangan}</p>
+          <p>{namaRsPkm}</p>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={6}>
+          <p>Kode RS/PKM</p>
+        </Col>
+        <Col span={18}>
+          <p>{kodeRsPkm}</p>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={6}>
+          <p>No. Urut Pencatatan Kematian Tiap Bulan</p>
+        </Col>
+        <Col span={18}>
+          <p>{nomorSurat}</p>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={6}>
+          <p>No. Rekam Medis</p>
+        </Col>
+        <Col span={18}>
+          <p>{nomorRekamMedis}</p>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={6}>
+          <p>Nama Penerima</p>
+        </Col>
+        <Col span={18}>
+          <p>{namaPenerima}</p>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={6}>
+          <p>Hubungan dengan Mendiang</p>
+        </Col>
+        <Col span={18}>
+          <p>{hubunganPenerima}</p>
         </Col>
       </Row>
     </Card>
