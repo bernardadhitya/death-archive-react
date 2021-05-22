ceda5e33dda80500cbc3d0c265f73052095fe89a
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-05-22 16:29:42 +0700

    [Bernard] User to export styled skmk spreadsheet

--- src/exporter.js
+++ src/exporter.js
@@ -33,14 +33,29 @@ export const exportSkmkLogByDate = async (startDate, endDate) => {
   const worksheet = workbook.addWorksheet('skmk');
   worksheet.state = 'visible';
 
+  worksheet.properties.defaultRowHeight = 21;
+
+  const borderStyle = {
+    top: { style: 'medium' },
+    left: { style: 'medium' },
+    bottom: { style: 'medium' },
+    right: { style: 'medium' },
+  }
+
+  const alignmentStyle = {
+    vertical: 'middle',
+    horizontal: 'center'
+  }
+
   worksheet.columns = [
-    { header: 'Nama', key: 'nama_jenazah', width: 32 },
-    { header: 'Jenis Kelamin', key: 'jenis_kelamin', width: 32 },
-    { header: 'Umur (tahun)', key: 'umur_tahun', width: 10 },
-    { header: 'Umur (bulan)', key: 'umur_bulan', width: 10 },
-    { header: 'Kecamatan', key: 'alamat_kecamatan', width: 10 },
-    { header: 'Kelurahan', key: 'alamat_kelurahan', width: 10 },
-    { header: 'Tanggal Meninggal', key: 'tanggal_meninggal', width: 32 },
+    { header: 'No.', key: 'idx', width: 10 },
+    { header: 'Nama', key: 'nama_jenazah', width: 24 },
+    { header: 'Jenis Kelamin', key: 'jenis_kelamin', width: 18 },
+    { header: 'Umur (tahun)', key: 'umur_tahun', width: 14 },
+    { header: 'Umur (bulan)', key: 'umur_bulan', width: 14 },
+    { header: 'Kecamatan', key: 'alamat_kecamatan', width: 18 },
+    { header: 'Kelurahan', key: 'alamat_kelurahan', width: 18 },
+    { header: 'Tanggal Meninggal', key: 'tanggal_meninggal', width: 18 },
     { header: 'Dasar', key: 'penyebab_dasar_id', width: 10 },
     { header: 'Antara 1', key: 'penyebab_antara_1_id', width: 10 },
     { header: 'Antara 2', key: 'penyebab_antara_2_id', width: 10 },
@@ -49,7 +64,56 @@ export const exportSkmkLogByDate = async (startDate, endDate) => {
     { header: 'Yg Menandatangani', key: 'nama_penandatangan', width: 32 }
   ];
 
-  filteredLogsByDateRange.map(log => worksheet.addRow(log));
+  const columnHeaders = {
+    'idx': 'No.',
+    'nama_jenazah': 'Nama',
+    'jenis_kelamin': 'Jenis Kelamin', 
+    'umur_tahun': 'Umur (tahun)', 
+    'umur_bulan': 'Umur (bulan)', 
+    'alamat_kecamatan': 'Kecamatan', 
+    'alamat_kelurahan': 'Kelurahan', 
+    'tanggal_meninggal': 'Tanggal Meninggal', 
+    'penyebab_dasar_id': 'Dasar', 
+    'penyebab_antara_1_id': 'Antara 1', 
+    'penyebab_antara_2_id': 'Antara 2', 
+    'penyebab_langsung_id': 'Langsung', 
+    'nama_pembuat_surat': 'Petugas Verifikator', 
+    'nama_penandatangan': 'Yg Menandatangani' 
+  }
+
+  worksheet.addRow(columnHeaders);
+  worksheet.addRow(columnHeaders);
+
+  filteredLogsByDateRange.forEach((log, idx) => {
+    worksheet.addRow({idx, ...log});
+  });
+
+  worksheet.mergeCells('A1:N1'); worksheet.getCell('A1').value = 'Register SKMK';
+
+  worksheet.mergeCells('A2:A3');
+  worksheet.mergeCells('B2:B3');
+  worksheet.mergeCells('C2:C3');
+  worksheet.mergeCells('D2:E2'); worksheet.getCell('D2').value = 'Umur'; //umur tahun dan bulan
+  worksheet.mergeCells('F2:F3');
+  worksheet.mergeCells('G2:G3');
+  worksheet.mergeCells('H2:H3');
+  worksheet.mergeCells('I2:L2'); worksheet.getCell('I2').value = 'Diagnosa'; //diagnosa
+  worksheet.mergeCells('M2:M3');
+  worksheet.mergeCells('N2:N3');
+
+  const cellName = (col, row) => {
+    return `${String.fromCharCode(65 + col)}${row + 1}`
+  }
+
+  worksheet.getCell('A1').font = { size: 14 }
+
+  for (let i=0; i < worksheet.columnCount; i++) {
+    for (let j=0; j < worksheet.rowCount; j++) {
+      worksheet.getCell(`${cellName(i, j)}`).alignment = alignmentStyle;
+      if (j+1 === 1) continue;
+      worksheet.getCell(`${cellName(i, j)}`).border = borderStyle;
+    }
+  };
 
   //await workbook.xlsx.writeBuffer('skmk-log');
 
